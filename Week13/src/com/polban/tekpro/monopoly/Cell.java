be461201a59b45889d84dff82479c7ee3fdb26fb
mentarialysias <97450819+mentarialysias@users.noreply.github.com>
2022-05-23 03:51:02 +0700

    Add files via upload

---
+++ Week13/src/com/polban/tekpro/monopoly/Cell.java
@@ -0,0 +1,41 @@
+package com.polban.tekpro.monopoly;
+
+public abstract class Cell {
+	private boolean available = true;
+	private String name;
+	protected Player owner;
+
+	public String getName() {
+		return name;
+	}
+
+	public Player getPlayer() {
+		return owner;
+	}
+	
+	public int getPrice() {
+		return 0;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+	
+	public abstract void playAction();
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	
+	void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPlayer(Player player) {
+		this.owner = player;
+	}
+    
+    public String toString() {
+        return name;
+    }
+}
